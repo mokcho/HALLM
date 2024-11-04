124b0212894d4f9a0355af234dab3c9415629277
speechunlearn <perla0328@g.skku.edu>
2024-11-03 21:35:58 -0500

    Update clotho.sh
    Updated to working paths / latest version of Clotho

--- data/prepare/clotho.sh
+++ data/prepare/clotho.sh
@@ -6,22 +6,28 @@ mkdir -p ./data/Clotho/audio
 TARGET_DIR="./data/Clotho"
 CAPTION_DIR="./data/Clotho/caption"
 
-DEV_URL="https://zenodo.org/records/3490684/files/clotho_audio_development.7z?download=1"
-EVAL_URL="https://zenodo.org/records/3490684/files/clotho_audio_evaluation.7z?download=1"
-DEV_CAP_URL="https://zenodo.org/records/3490684/files/clotho_captions_development.csv?download=1"
-EVAL_CAP_URL="https://zenodo.org/records/3490684/files/clotho_captions_evaluation.csv?download=1"
-DEV_META_URL="https://zenodo.org/records/3490684/files/clotho_metadata_development.csv?download=1"
-EVAL_META_URL="https://zenodo.org/records/3490684/files/clotho_metadata_evaluation.csv?download=1"
+DEV_URL="https://zenodo.org/records/4783391/files/clotho_audio_development.7z?download=1"
+EVAL_URL="https://zenodo.org/records/4783391/files/clotho_audio_evaluation.7z?download=1"
+VAL_URL="https://zenodo.org/records/4783391/files/clotho_audio_validation.7z?download=1"
+DEV_CAP_URL="https://zenodo.org/records/4783391/files/clotho_captions_development.csv?download=1"
+EVAL_CAP_URL="https://zenodo.org/records/4783391/files/clotho_captions_evaluation.csv?download=1"
+VAL_CAP_URL="https://zenodo.org/records/4783391/files/clotho_captions_validation.csv?download=1"
+DEV_META_URL="https://zenodo.org/records/4783391/files/clotho_metadata_development.csv?download=1"
+EVAL_META_URL="https://zenodo.org/records/4783391/files/clotho_metadata_evaluation.csv?download=1"
+VAL_META_URL="https://zenodo.org/records/4783391/files/clotho_metadata_validation.csv?download=1"
 
 
 # Download the file
 echo "Downloading Clotho dataset..."
-wget -O "$TARGET_DIR/dev_metadata.csv" "$EVAL_META_URL"
-wget -O "$TARGET_DIR/eval_metadata.csv" "$DEV_META_URL"
+wget -O "$TARGET_DIR/dev_metadata.csv" "$DEV_META_URL"
+wget -O "$TARGET_DIR/eval_metadata.csv" "$EVAL_META_URL"
+wget -O "$TARGET_DIR/val_metadata.csv" "$VAL_META_URL"
 wget -O "$CAPTION_DIR/eval.csv" "$EVAL_CAP_URL"
 wget -O "$CAPTION_DIR/dev.csv" "$DEV_CAP_URL"
+wget -O "$CAPTION_DIR/val.csv" "$VAL_CAP_URL"
 wget -O "$TARGET_DIR/clotho_audio_development.7z" "$DEV_URL"
 wget -O "$TARGET_DIR/clotho_audio_evaluation.7z" "$EVAL_URL"
+wget -O "$TARGET_DIR/clotho_audio_validation.7z" "$VAL_URL
 
 # Inform the user
 if [ $? -eq 0 ]; then
@@ -37,3 +43,4 @@ brew install p7zip #macOS using homebrew
 
 7z x "$TARGET_DIR/clotho_audio_development.7z" -o./data/Clotho/audio/
 7z x "$TARGET_DIR/clotho_audio_evaluation.7z" -o./data/Clotho/audio/
+7z x "$TARGET_DIR/clotho_audio_validation.7z" -o./data/Clotho/audio/
